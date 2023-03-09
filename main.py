d1f82185233ed28375bb5f521feff60d1fb05a18
actuallypav <actuallypav@gmail.com>
2023-03-09 01:26:38 +0000

    caps at 8, and changes to another screen

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import kivy
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.config import Config
 from kivy.lang import Builder
 
-#setting the app to resizable
+#setting the app to resizable/fullscreen
 Config.set('graphics', 'resizable', 1)
-Config.set('graphics', 'fullscreen', 1)
-Config.set('graphics', 'borderless', 1)
+#Config.set('graphics', 'fullscreen', 1)
+#Config.set('graphics', 'borderless', 1)
 
+#new screenmanager
+sm = ScreenManager()
 
+class ReactionGridLayout(GridLayout):
+    pass
+
+#this is the logon 
 class NumpadGridLayout(GridLayout):
 
     #function called on logon button pressed
-    def logon(self, logon):
-        if logon:
+    def sessionID(self, sessionID):
+        if sessionID:
             try:
                 #attempt to login, otherwise throw error
-                print(logon)
+                print(sessionID)
+                # replace the current grid layout with the ReactionGridLayout
+                self.parent.add_widget(ReactionGridLayout())
+                self.parent.remove_widget(self)
             except:
                 self.display.text = 'ERROR'
 
@@ -31,7 +41,17 @@ class SmileyApp(App):
             Builder.load_file("main.kv")
         except Exception as e:
             print('failed to load .kv file. this is the error: ', e)
-        return NumpadGridLayout()
+
+        # add a new screen and set it as the current screen
+        main_screen = Screen(name='main')
+        sm.add_widget(main_screen)
+        sm.current = 'main'
+
+        # add the numpad grid layout to the current screen
+        numpad_grid_layout = NumpadGridLayout()
+        main_screen.add_widget(numpad_grid_layout)
+
+        return sm
 
 smileyApp = SmileyApp()
 smileyApp.run()
